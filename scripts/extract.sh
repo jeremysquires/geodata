81d04f714a38d7ed4d938322861e0d74517562d9
Jeremy Squires SensorUp <jeremy.squires@sensorup.com>
2019-11-25 10:25:29 -0700

    make scripts more generic

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,27 +1,39 @@
-head -q -n 10 header.csv ais_sorted.csv > h.csv
-node ~/Code/geodata/src/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
-node ~/Code/geodata/src/geojson2shipsim.js -o pos10.json h.json
-head -q -n 100 header.csv ais_sorted.csv > h.csv
-node ~/Code/geodata/src/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
-node ~/Code/geodata/src/geojson2shipsim.js -o pos100.json h.json
-head -q -n 1000 header.csv ais_sorted.csv > h.csv
-node ~/Code/geodata/src/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
-node ~/Code/geodata/src/geojson2shipsim.js -o pos1000.json h.json
-head -q -n 10000 header.csv ais_sorted.csv > h.csv
-node ~/Code/geodata/src/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
-node ~/Code/geodata/src/geojson2shipsim.js -o pos10000.json h.json
-head -q -n 100000 header.csv ais_sorted.csv > h.csv
-node ~/Code/geodata/src/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
-node ~/Code/geodata/src/geojson2shipsim.js -o pos100000.json h.json
-head -q -n 1000000 header.csv ais_sorted.csv > h.csv
-node ~/Code/geodata/src/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
-node ~/Code/geodata/src/geojson2shipsim.js -o pos1000000.json h.json
-head -q -n 10000000 header.csv ais_sorted.csv > h.csv
-node ~/Code/geodata/src/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
-node ~/Code/geodata/src/geojson2shipsim.js -o pos10000000.json h.json
-head -q -n 20000000 header.csv ais_sorted.csv > h.csv
-node ~/Code/geodata/src/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
-node ~/Code/geodata/src/geojson2shipsim.js -o pos20000000.json h.json
-head -q -n 30000000 header.csv ais_sorted.csv > h.csv
-node ~/Code/geodata/src/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
-node ~/Code/geodata/src/geojson2shipsim.js -o pos30000000.json h.json
+GEODATASCRIPTS=`dirname "$0"`
+GEODATASRC=$GEODATASCRIPTS/../src
+if [ ! -f $GEODATASRC/csv2geojson.js ]; then
+  echo $GEODATASRC is not the path to the geodata package
+  exit 0
+fi
+HEADER=header.csv
+if [ ! -f $HEADER ]; then
+  HEADER=$GEODATASCRIPTS/header.csv
+fi
+
+head -n 10 $HEADER ais_sorted.csv > h.csv
+node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
+node $GEODATASRC/geojson2shipsim.js -o pos10.json h.json
+exit 1
+head -n 100 $HEADER ais_sorted.csv > h.csv
+node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
+node $GEODATASRC/geojson2shipsim.js -o pos100.json h.json
+head -n 1000 $HEADER ais_sorted.csv > h.csv
+node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
+node $GEODATASRC/geojson2shipsim.js -o pos1000.json h.json
+head -n 10000 $HEADER ais_sorted.csv > h.csv
+node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
+node $GEODATASRC/geojson2shipsim.js -o pos10000.json h.json
+head -n 100000 $HEADER ais_sorted.csv > h.csv
+node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
+node $GEODATASRC/geojson2shipsim.js -o pos100000.json h.json
+head -n 1000000 $HEADER ais_sorted.csv > h.csv
+node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
+node $GEODATASRC/geojson2shipsim.js -o pos1000000.json h.json
+head -n 10000000 $HEADER ais_sorted.csv > h.csv
+node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
+node $GEODATASRC/geojson2shipsim.js -o pos10000000.json h.json
+head -n 20000000 $HEADER ais_sorted.csv > h.csv
+node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
+node $GEODATASRC/geojson2shipsim.js -o pos20000000.json h.json
+head -n 30000000 $HEADER ais_sorted.csv > h.csv
+node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
+node $GEODATASRC/geojson2shipsim.js -o pos30000000.json h.json
