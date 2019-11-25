b27e887665a6d94a8807932f9008ade7e3101cd5
Jeremy Squires SensorUp <jeremy.squires@sensorup.com>
2019-11-25 10:50:07 -0700

    deal with lack of -q opt on head in macos

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -12,27 +12,27 @@ fi
 head -n 10 $HEADER ais_sorted.csv | grep -v == > h.csv
 node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
 node $GEODATASRC/geojson2shipsim.js -o pos10.json h.json
-head -n 100 $HEADER ais_sorted.csv > h.csv
+head -n 100 $HEADER ais_sorted.csv | grep -v == > h.csv
 node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
 node $GEODATASRC/geojson2shipsim.js -o pos100.json h.json
-head -n 1000 $HEADER ais_sorted.csv > h.csv
+head -n 1000 $HEADER ais_sorted.csv | grep -v == > h.csv
 node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
 node $GEODATASRC/geojson2shipsim.js -o pos1000.json h.json
-head -n 10000 $HEADER ais_sorted.csv > h.csv
+head -n 10000 $HEADER ais_sorted.csv | grep -v == > h.csv
 node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
 node $GEODATASRC/geojson2shipsim.js -o pos10000.json h.json
-head -n 100000 $HEADER ais_sorted.csv > h.csv
+head -n 100000 $HEADER ais_sorted.csv | grep -v == > h.csv
 node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
 node $GEODATASRC/geojson2shipsim.js -o pos100000.json h.json
-head -n 1000000 $HEADER ais_sorted.csv > h.csv
+head -n 1000000 $HEADER ais_sorted.csv | grep -v == > h.csv
 node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
 node $GEODATASRC/geojson2shipsim.js -o pos1000000.json h.json
-head -n 10000000 $HEADER ais_sorted.csv > h.csv
+head -n 10000000 $HEADER ais_sorted.csv | grep -v == > h.csv
 node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
 node $GEODATASRC/geojson2shipsim.js -o pos10000000.json h.json
-head -n 20000000 $HEADER ais_sorted.csv > h.csv
+head -n 20000000 $HEADER ais_sorted.csv | grep -v == > h.csv
 node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
 node $GEODATASRC/geojson2shipsim.js -o pos20000000.json h.json
-head -n 30000000 $HEADER ais_sorted.csv > h.csv
+head -n 30000000 $HEADER ais_sorted.csv | grep -v == > h.csv
 node $GEODATASRC/csv2geojson.js -o h.json --lat LAT --lon LON --numeric-fields SOG,COG,Heading,Length,Width,Draft h.csv
 node $GEODATASRC/geojson2shipsim.js -o pos30000000.json h.json
